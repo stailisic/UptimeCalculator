f9f7c0c0796c40b8013f2276a20c4a3af03087d5
Anna V. Nagel <annavnaglia@gmail.com>
2023-04-13 13:19:29 +0200

    init

---
+++ src/main/java/com/example/uptimecalculator/UptimeCalculatorApplication.java
@@ -0,0 +1,15 @@
+package com.example.uptimecalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UptimeCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UptimeCalculatorApplication.class, args);
+
+
+    }
+
+}
---
+++ src/test/java/com/example/uptimecalculator/UptimeCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uptimecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UptimeCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
